f29384973141d3d4a972d09b1db43ee99de21af5
Randall Coding <randallcoding@protonmail.com>
2023-12-14 15:10:21 +0700

    Updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 acorn build -t ghcr.io/randall-coding/acorn/gogs && \
 acorn push ghcr.io/randall-coding/acorn/gogs && \
-acorn run -n gogs ghcr.io/randall-coding/acorn/gogs
+acorn run -n gogs -s gogs-database:gogs-database --memory=1Gi ghcr.io/randall-coding/acorn/gogs
